bf78aa908e742cec0e3d5dfa9bf7e05209abf7a9
SE140046\Trung Tuyển <trungtuyen00310@gmail.com>
2021-10-21 22:38:03 +0700

    requiredInput

--- flutter_application_2/lib/Screens/register/register.dart
+++ flutter_application_2/lib/Screens/register/register.dart
@@ -1,109 +1,172 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:job/Screens/login/login.dart';
 import 'package:job/components/background.dart';
+import 'package:job/views/home.dart';
 
 class RegisterScreen extends StatelessWidget {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Background(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Text(
-                "ĐĂNG KÝ",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF2661FA),
-                    fontSize: 36),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            SizedBox(height: size.height * 0.03),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(labelText: "Họ và tên"),
-              ),
-            ),
-            SizedBox(height: size.height * 0.03),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(labelText: "Số điện thoại"),
-              ),
-            ),
-            SizedBox(height: size.height * 0.03),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(labelText: "Tên tài khoản"),
-              ),
-            ),
-            SizedBox(height: size.height * 0.03),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(labelText: "Mật khẩu"),
-                obscureText: true,
-              ),
-            ),
-            SizedBox(height: size.height * 0.05),
-            Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: RaisedButton(
-                onPressed: () {},
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(80.0)),
-                textColor: Colors.white,
-                padding: const EdgeInsets.all(0),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 50.0,
-                  width: size.width * 0.5,
-                  decoration: new BoxDecoration(
-                      borderRadius: BorderRadius.circular(80.0),
-                      gradient: new LinearGradient(colors: [
-                        Color.fromARGB(255, 255, 136, 34),
-                        Color.fromARGB(255, 255, 177, 41)
-                      ])),
-                  padding: const EdgeInsets.all(0),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Background(
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.symmetric(horizontal: 40),
                   child: Text(
                     "ĐĂNG KÝ",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF2661FA),
+                        fontSize: 36),
+                    textAlign: TextAlign.left,
                   ),
                 ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: GestureDetector(
-                onTap: () => {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()))
-                },
-                child: Text(
-                  "Đã có tài khoản? Đăng nhập",
-                  style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF2661FA)),
+                SizedBox(height: size.height * 0.03),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: "Họ và tên *"),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Vui lòng không được để trống";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
                 ),
-              ),
-            )
-          ],
+                SizedBox(height: size.height * 0.03),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: "Số điện thoại *"),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Vui lòng không được để trống";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(height: size.height * 0.03),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: "Tên tài khoản *"),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Vui lòng không được để trống";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(height: size.height * 0.03),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: "Mật khẩu *"),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Vui lòng không được để trống";
+                      } else {
+                        return null;
+                      }
+                    },
+                    obscureText: true,
+                  ),
+                ),
+                SizedBox(height: size.height * 0.03),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextFormField(
+                    decoration:
+                        InputDecoration(labelText: "Nhập lại mật khẩu *"),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Vui lòng không được để trống";
+                      } else {
+                        return null;
+                      }
+                    },
+                    obscureText: true,
+                  ),
+                ),
+                SizedBox(height: size.height * 0.05),
+                Container(
+                  alignment: Alignment.centerRight,
+                  margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Home()));
+                      } else {}
+                    },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0)),
+                    textColor: Colors.white,
+                    padding: const EdgeInsets.all(0),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 50.0,
+                      width: size.width * 0.5,
+                      decoration: new BoxDecoration(
+                          borderRadius: BorderRadius.circular(80.0),
+                          gradient: new LinearGradient(colors: [
+                            Color.fromARGB(255, 255, 136, 34),
+                            Color.fromARGB(255, 255, 177, 41)
+                          ])),
+                      padding: const EdgeInsets.all(0),
+                      child: Text(
+                        "ĐĂNG KÝ",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  child: GestureDetector(
+                    onTap: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginScreen()))
+                    },
+                    child: Text(
+                      "Đã có tài khoản? Đăng nhập",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF2661FA)),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
